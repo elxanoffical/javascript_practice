6271fe4ddedb646577b0bf905e0ca788b2372e51
unknown <elxan.offical.07@gmail.com>
2023-07-06 11:54:25 +0400

    lesson2 finish

---
+++ lesson1/app.js
@@ -0,0 +1,17 @@
+
+
+
+
+student1 = 'Elxan'
+student2 = 'Fatime'
+student3 = 'Jabbar'
+
+
+
+console.log(student1);
+console.log(student2);
+console.log(student3);
+
+
+students = [ student1 , student2 , student3 ]
+console.log(students);
---
+++ lesson2/app.js
@@ -0,0 +1,117 @@
+/*
+// typeof - value-nun datatype tapir
+console.log(typeof(true));
+console.log(typeof(5.2));
+console.log(typeof('mamed'));
+*/
+
+/*
+// Yazilis stili 
+
+// camelCase
+let macbookPrice = 36
+
+// snakelCase
+let macbook_Price_1 = 36
+
+// PascalCase
+let MacbookPrice = 36
+*/
+
+/*
+let name = 'Elxan' , age = 18
+
+console.log(`hi my name is ${name} I am ${age} years old`);
+*/
+
+
+
+/*
+// arifmetich operators
+let a = 30 ,b = 7
+
+// additon (+)
+let result1 = a + b
+console.log(`${a} + ${b} = ${result1}`)
+
+// subtraction (-)
+let result2 = a - b
+console.log(`${a} - ${b} = ${result2}`)
+
+
+// multiplicatin (*) vurma
+let result3 = a * b
+console.log(`${a} * ${b} = ${result3}`)
+
+
+// divistion (/)  bolme
+let result4 = a / b
+console.log(`${a} / ${b} = ${result4}`)
+
+
+// exponentiation (**) quvvet
+let result5 = a ** b
+console.log(`${a} ** ${b} = ${result5}`)
+
+
+// modul (%) qaliq  qaligi tap
+let result6 = a % b
+console.log(`${a} % ${b} = ${result6}`)
+*/
+
+
+
+
+// Asigment Operators (beraberlesdirme)
+
+let a = 22, b = 3
+
+// +=  Soldaki ededi sagdaki qeder artir (soldaki eded deyisir)
+// a+=b  |  a ededini b qeder artir (a ededi deyisir/artir)
+// b+=a  |  b ededini a qeder artir (b ededi deyisir/artir)
+
+/*
+console.log(`a: ${a}`);
+console.log(`b: ${b}`);
+a+=b  // 22+=3  | 22 ededi 3 vahid artdi, 22 ededi deyisdi
+console.log('a+=b');
+console.log(`a: ${a}`);
+console.log(`b: ${b}`);
+*/ 
+
+
+/*
+console.log(`a: ${a} , b: ${b}`);
+a+=b  // 22-=3  | 22 ededi 3 vahid azaldi, 22 ededi deyisdi
+console.log('a-=b');
+console.log(`a: ${a} , b: ${b}`);
+*/
+
+
+
+/*
+console.log(`a: ${a} , b: ${b}`);
+b*=a  // 3*=22  | 3 ededi 22 defe artsin, 3 ededi deyisdi
+console.log('b*=a');
+console.log(`a: ${a} , b: ${b}`);
+*/
+
+
+
+/*
+let a =5, b = 10
+console.log(`${a} < ${b} = ${a < b}`);
+console.log(`${a} > ${b} = ${a > b}`);
+*/
+
+
+// >= boyuk veya beraberdirmi
+// <= kicikdir veya beraberdirmi
+
+
+
+
+
+
+
+//logical opertatur
